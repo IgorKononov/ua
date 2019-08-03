c74848948bbcd11ec3b73b9460c03834302a4a42
Skeeter <kononov.igor@gmail.com>
2019-08-03 19:08:18 +0300

    Various translation updates, fixes, update button added

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@ return array(
     'name' => 'Shop-Script по-украински',
     'description'  => 'Перевод на украинский язык интерфейса Shop-Script и других продуктов Webasyst',
     'img' => 'img/ua.png',
-    'version' => '0.1',
+    'version' => '0.3',
     'vendor' => '1077750',
     'custom_settings' => true,
 );
