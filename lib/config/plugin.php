d74e07ff2fcb367bcc9deeae0cfd6f6cc9e25466
Skeeter <kononov.igor@gmail.com>
2019-10-21 23:30:05 +0300

    Version info update, tar.gz update

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@ return array(
     'name' => 'Shop-Script по-украински',
     'description'  => 'Перевод на украинский язык интерфейса Shop-Script и других продуктов Webasyst',
     'img' => 'img/ua.png',
-    'version' => '1.1',
+    'version' => '1.2',
     'vendor' => '1077750',
     'custom_settings' => true,
 );
