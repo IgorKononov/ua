9dd193e4b3a641c71f5785c1b7d9f0d11396865e
IgorKononov <kononov.igor@gmail.com>
2020-05-13 12:48:00 +0300

    Update 1.5

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@ return array(
     'name' => 'Shop-Script по-украински',
     'description'  => 'Перевод на украинский язык интерфейса Shop-Script и других продуктов Webasyst',
     'img' => 'img/ua.png',
-    'version' => '1.4',
+    'version' => '1.5',
     'vendor' => '1077750',
     'custom_settings' => true,
 );
