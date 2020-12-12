529ac6535ca8f788ed57eb6f7b1413f5603ff691
IgorKononov <kononov.igor@gmail.com>
2020-12-12 11:55:48 +0300

    1.6.0 Release version
    Webasyst 1.14.9.567
    Webasyst Installer 1.14.9.567
    Webasyst Shop-Script 8.15.0.40
    Webasyst Site 2.5.20.234
    Webasyst Team 1.1.5.146
    1312 Inc. Pocket Lists 3.7.0.p
    1312 Inc. Tasks 1.3.0.105

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@ return array(
     'name' => 'Shop-Script по-украински',
     'description'  => 'Перевод на украинский язык интерфейса Shop-Script и других продуктов Webasyst',
     'img' => 'img/ua.png',
-    'version' => '1.5.2',
+    'version' => '1.6.0',
     'vendor' => '1077750',
     'custom_settings' => true,
 );
